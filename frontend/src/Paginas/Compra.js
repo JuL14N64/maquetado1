97e03e1be961ee025eeb4f5a61e34f99b8cdf9ea
Gabriel <22393278@utcancun.edu.mx>
2023-07-20 02:41:15 -0500

    se agrego un inicio mas bonito

--- frontend/src/Paginas/Compra.js
+++ frontend/src/Paginas/Compra.js
@@ -1,59 +1,52 @@
-
-
 import React from "react";
-
-import Encabezado from "../Componentes/Encabezado";
-
 import "../css/Compra.css";
 
-//2 
+import Encabezado from "../Componentes/Encabezado";
+import PagoImage from "../images/pago.png";
 
 function Compra() {
-    //3
-    return (
-        <>
-            <Encabezado />
+  return (
+    <>
+      <Encabezado />
 
-          
+      <main>
+        <div id="formulario">
+          <div className="campo">
+            <label htmlFor="nombre-apellido">Nombre y Apellido:</label>
+            <input type="text" id="nombre-apellido" placeholder="Ingrese su nombre y apellido" />
+          </div>
+          <div className="campo">
+            <label htmlFor="numero">Número:</label>
+            <input type="text" id="numero" placeholder="Ingrese su número de teléfono" />
+          </div>
+          <div className="campo">
+            <label htmlFor="numero-tarjeta">Número de Tarjeta:</label>
+            <input type="text" id="numero-tarjeta" placeholder="Ingrese el número de su tarjeta" />
+          </div>
+          <div className="campo">
+            <label htmlFor="cvv">CVV:</label>
+            <input type="text" id="cvv" placeholder="Ingrese el CVV de su tarjeta" />
+          </div>
+          <button id="comprar">Comprar ahora</button>
+        </div>
 
-            <main>
-                <div id="formulario">
-                    <div className="campo">
-                        <label for="nombre-apellido">Nombre y Apellido:</label>
-                        <input type="text" id="nombre-apellido" placeholder="Ingrese su nombre y apellido"></input>
-                    </div>
-                    <div className="campo">
-                        <label for="numero">Número:</label>
-                        <input type="text" id="numero" placeholder="Ingrese su número de teléfono"></input>
-                    </div>
-                    <div className="campo">
-                        <label for="numero-tarjeta">Número de Tarjeta:</label>
-                        <input type="text" id="numero-tarjeta" placeholder="Ingrese el número de su tarjeta"></input>
-                    </div>
-                    <div className="campo">
-                        <label for="cvv">CVV:</label>
-                        <input type="text" id="cvv" placeholder="Ingrese el CVV de su tarjeta"></input>
-                    </div>
-                    <button id="comprar">Comprar ahora</button>
-                </div>
-                <h1 id="mensaje">DISFRUTA TU PRODUCTO!</h1>
+        <h1 id="mensaje">DISFRUTA TU PRODUCTO!</h1>
 
-                <p id="textopago">En nuestra página web, entendemos la importancia de ofrecerte una experiencia de compra fluida
-                    y segura desde el inicio hasta el final. Por eso, hemos desarrollado un sistema de pago en línea confiable y
-                    de vanguardia que te permitirá realizar tus transacciones de manera segura y sin complicaciones.</p>
+        <p id="textopago">
+          En nuestra página web, entendemos la importancia de ofrecerte una experiencia de compra fluida
+          y segura desde el inicio hasta el final. Por eso, hemos desarrollado un sistema de pago en línea confiable y
+          de vanguardia que te permitirá realizar tus transacciones de manera segura y sin complicaciones.
+        </p>
 
-                <img src={require("../images/pago.png")} alt="Pago" id="pago"></img>
-            </main>
+        <img src={PagoImage} alt="Pago" id="pago" />
+      </main>
 
-            <footer>
-                <p>Copyright © 2023 CITHE tools and machines</p>
-                <p>Correo: cithe@experts.com Telefono: 899736212</p>
-            </footer>
-
-        </>
-    )
+      <footer>
+        <p>Copyright © 2023 CITHE tools and machines</p>
+        <p>Correo: cithe@experts.com Telefono: 899736212</p>
+      </footer>
+    </>
+  );
 }
-
-//4
 
 export default Compra;
--- frontend/src/Paginas/Inicio.js
+++ frontend/src/Paginas/Inicio.js
@@ -21,34 +21,220 @@ function Inicio() {
 
              
                 <main>
-                    <div id="titulo">
-                        <h1>EXPERTOS EN EQUIPO DE HERRAMIENTAS Y CONSTRUCCION</h1>
-                    </div>
-                    <div id="sub">
-                        <h1>CITE: Innovación y calidad en herramientas para profesionales</h1>
-                    </div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.</p>
-                    <div id="sub2">
-                        <p>En CITE, no solo nos preocupamos por brindar productos de alta calidad, sino también por ofrecer un
-                            servicio excepcional a nuestros clientes. Nuestro equipo de profesionales altamente capacitados y
-                            comprometidos está siempre dispuesto a asesorar y brindar soluciones personalizadas, garantizando una
-                            experiencia integral y satisfactoria. Además, nos esforzamos por mantenernos a la vanguardia de la
-                            industria, invirtiendo en investigación y desarrollo para seguir innovando y ofreciendo soluciones que
-                            impulsen el éxito de nuestros clientes.
+                 <div
+    className="modal fade bg-white"
+    id="templatemo_search"
+    tabIndex={-1}
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div className="modal-dialog modal-lg" role="document">
+      <div className="w-100 pt-1 mb-5 text-right">
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        />
+      </div>
+      <form
+        action=""
+        method="get"
+        className="modal-content modal-body border-0 p-0"
+      >
+        <div className="input-group mb-2">
+          <input
+            type="text"
+            className="form-control"
+            id="inputModalSearch"
+            name="q"
+            placeholder="Search ..."
+          />
+          <button
+            type="submit"
+            className="input-group-text bg-success text-light"
+          >
+            <i className="fa fa-fw fa-search text-white" />
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  {/* Start Banner Hero */}
+  <section className="template-mo-zay-hero-carousel">
+  <div class="container">
+    <div class="row p-5">
+        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+        <img src={require("../images/banner_img_01.png")} alt="bannimg" id="bannimg"></img>
+        </div>
+        <div class="col-lg-6 mb-0 d-flex align-items-center">
+            <div class="text-align-left align-self-center">
+                <h1 class="titulo-tienda"><b>CITHE</b> FERRETERIA</h1>
+                <h3 class="h2">La mejor distribuidora de herramientas en línea</h3>
+                <p>
+                    Cithe, tu destino ideal para todas tus necesidades de herramientas y materiales de construcción.
+                    Somos una ferretería comprometida con ofrecer productos de alta calidad y un servicio excepcional
+                    tanto a profesionales del sector como a aficionados del bricolaje. Nuestra amplia selección de productos y
+                    accesorios te permitirá llevar a cabo tus proyectos con facilidad y éxito.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+  </section>
+  {/* End Banner Hero */}
+  {/* Start Categories of The Month */}
+  <section className="container py-5">
+    <div className="row text-center pt-3">
+      <div className="col-lg-6 m-auto">
+        <h1 className="h1">Categorias del mes</h1>
+        <p>
+          Organizamos nuestros productos en categorías para que puedas encontrar
+          rápidamente lo que necesitas.
+        </p>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-12 col-md-4 p-5 mt-3">
+        <a href="#">
+        <img src={require("../images/category_img_01.jpg")} alt="Categoriaimg" id="categoriaimg"></img>
+        </a>
+        <h5 className="text-center mt-3 mb-3">Electricas</h5>
+        <p className="text-center">
+          <a className="btn btn-success">Go Shop</a>
+        </p>
+      </div>
+      <div className="col-12 col-md-4 p-5 mt-3">
+        <a href="#">
+        <img src={require("../images/category_img_02.jpg")} alt="Categoriaimg2" id="categoriaimg2"></img>
+        </a>
+        <h2 className="h5 text-center mt-3 mb-3">Manuales</h2>
+        <p className="text-center">
+          <a className="btn btn-success">Go Shop</a>
+        </p>
+      </div>
+      <div className="col-12 col-md-4 p-5 mt-3">
+        <a href="#">
+        <img src={require("../images/category_img_03.jpg")} alt="Categoriaimg3" id="categoriaimg3"></img> 
+        </a>
+        <h2 className="h5 text-center mt-3 mb-3">De Golpe</h2>
+        <p className="text-center">
+          <a className="btn btn-success">Go Shop</a>
+        </p>
+      </div>
+    </div>
+  </section>
+  {/* End Categories of The Month */}
+  {/* Start Featured Product */}
+  <section className="bg-light">
+    <div className="container py-5">
+      <div className="row text-center py-3">
+        <div className="col-lg-6 m-auto">
+          <h1 className="h1">Productos</h1>
+          <p>
+            En Cithe, entendemos lo importante que es para ti obtener tus
+            productos de forma rápida y sencilla.
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-4 mb-4">
+          <div className="card h-100">
+            <a href="shop-single.html">
+            <img src={require("../images/feature_prod_01.jpeg")} alt="Procimg1" id="procimg1"></img> 
+            </a>
+            <div className="card-body">
+              <ul className="list-unstyled d-flex justify-content-between">
+                <li>
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-muted fa fa-star" />
+                  <i className="text-muted fa fa-star" />
+                </li>
+                <li className="text-muted text-right">$240.00</li>
+              </ul>
+              <a
+                href="shop-single.html"
+                className="h2 text-decoration-none text-dark"
+              >
+                Slot 1
+              </a>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+                in culpa qui officia deserunt.
+              </p>
+              <p className="text-muted">Reviews (24)</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 mb-4">
+          <div className="card h-100">
+            <a href="shop-single.html">
+            <img src={require("../images/feature_prod_02.jpeg")} alt="Procimg2" id="procimg2"></img> 
+            </a>
+            <div className="card-body">
+              <ul className="list-unstyled d-flex justify-content-between">
+                <li>
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-muted fa fa-star" />
+                  <i className="text-muted fa fa-star" />
+                </li>
+                <li className="text-muted text-right">$480.00</li>
+              </ul>
+              <a
+                href="shop-single.html"
+                className="h2 text-decoration-none text-dark"
+              >
+                Slot 2
+              </a>
+              <p className="card-text">
+                Aenean gravida dignissim finibus. Nullam ipsum diam, posuere
+                vitae pharetra sed, commodo ullamcorper.
+              </p>
+              <p className="text-muted">Reviews (48)</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 mb-4">
+          <div className="card h-100">
+            <a href="shop-single.html">
+            <img src={require("../images/feature_prod_03.jpeg")} alt="Procimg3" id="procimg3"></img> 
+            </a>
+            <div className="card-body">
+              <ul className="list-unstyled d-flex justify-content-between">
+                <li>
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                  <i className="text-warning fa fa-star" />
+                </li>
+                <li className="text-muted text-right">$360.00</li>
+              </ul>
+              <a
+                href="shop-single.html"
+                className="h2 text-decoration-none text-dark"
+              >
+                Slot 3
+              </a>
+              <p className="card-text">
+                Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar
+                sagittis diam, et scelerisque ipsum lobortis nec.
+              </p>
+              <p className="text-muted">Reviews (74)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+ 
 
-                            Ya sea que necesite herramientas para la construcción, la industria automotriz, la carpintería o
-                            cualquier otro ámbito profesional, en CITE encontrará la combinación perfecta de calidad, durabilidad y
-                            rendimiento. Estamos orgullosos de ser reconocidos como un referente en el mercado de herramientas, y
-                            trabajamos arduamente para mantenernos a la altura de nuestra reputación. Confíe en CITE para equiparse
-                            con las mejores herramientas y lograr resultados excepcionales en cada proyecto.</p>
-                    </div>
-                    <img src={require("../images/soldando.png")} alt="Soldar" id="soldando"></img>
-                    <img src={require("../images/madera.png")} alt="Madera" id="madera"></img>
-
-                    
+             
                 </main>
                 <footer>
                 <p>Copyright © 2023 CITHE tools and machines</p>
